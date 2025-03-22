3f78cc8c2f1b0970cb07ffbb1648fe40186ef016
mfirhas <fathir.irhas@gmail.com>
2025-03-22 07:01:44 +0700

    Comment out forex impl api test

--- pfm-core/src/forex_impl/exchange_api_test.rs
+++ pfm-core/src/forex_impl/exchange_api_test.rs
@@ -1,52 +1,52 @@
-use chrono::{NaiveDate, TimeZone, Utc};
+// use chrono::{NaiveDate, TimeZone, Utc};
 
-use crate::forex::{ForexHistoricalRates, ForexRates};
+// use crate::forex::{ForexHistoricalRates, ForexRates};
 
-use super::exchange_api::Api;
-use crate::global::http_client;
+// use super::exchange_api::Api;
+// use crate::global::http_client;
 
-#[tokio::test]
-async fn test_rates() {
-    let client = http_client().clone();
-    let api = Api::new(client);
+// #[tokio::test]
+// async fn test_rates() {
+//     let client = http_client().clone();
+//     let api = Api::new(client);
 
-    let ret = api.rates(crate::forex::Currencies::USD).await;
+//     let ret = api.rates(crate::forex::Currencies::USD).await;
 
-    dbg!(&ret);
+//     dbg!(&ret);
 
-    assert!(ret.is_ok());
-}
+//     assert!(ret.is_ok());
+// }
 
-#[tokio::test]
-async fn test_historical_rates() {
-    let client = http_client().clone();
-    let api = Api::new(client);
+// #[tokio::test]
+// async fn test_historical_rates() {
+//     let client = http_client().clone();
+//     let api = Api::new(client);
 
-    let date = NaiveDate::from_ymd_opt(2024, 12, 20)
-        .unwrap()
-        .and_hms_opt(0, 0, 0)
-        .unwrap();
+//     let date = NaiveDate::from_ymd_opt(2024, 12, 20)
+//         .unwrap()
+//         .and_hms_opt(0, 0, 0)
+//         .unwrap();
 
-    // note: this return error as exchange_api historical data is very limited.
-    // source: https://github.com/fawazahmed0/exchange-api/issues/115
-    // let date = NaiveDate::from_ymd_opt(2022, 12, 20)
-    //     .unwrap()
-    //     .and_hms_opt(0, 0, 0)
-    //     .unwrap();
+//     // note: this return error as exchange_api historical data is very limited.
+//     // source: https://github.com/fawazahmed0/exchange-api/issues/115
+//     // let date = NaiveDate::from_ymd_opt(2022, 12, 20)
+//     //     .unwrap()
+//     //     .and_hms_opt(0, 0, 0)
+//     //     .unwrap();
 
-    dbg!(&date);
-    println!("{}", &date);
+//     dbg!(&date);
+//     println!("{}", &date);
 
-    let utc = Utc.from_utc_datetime(&date);
+//     let utc = Utc.from_utc_datetime(&date);
 
-    dbg!(&utc);
-    println!("{}", &utc);
+//     dbg!(&utc);
+//     println!("{}", &utc);
 
-    let ret = api
-        .historical_rates(utc, crate::forex::Currencies::USD)
-        .await;
+//     let ret = api
+//         .historical_rates(utc, crate::forex::Currencies::USD)
+//         .await;
 
-    dbg!(&ret);
+//     dbg!(&ret);
 
-    assert!(ret.is_ok());
-}
+//     assert!(ret.is_ok());
+// }
