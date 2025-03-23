1400bc1f928c65f1dae73518ce5313b54a8b41cf
mfirhas <fathir.irhas@gmail.com>
2025-03-23 09:31:05 +0700

    Refactor pfm-tool to pull historical data from different apis

--- pfm-tool/src/main.rs
+++ pfm-tool/src/main.rs
@@ -1,8 +1,7 @@
 use chrono::{DateTime, Datelike, TimeDelta, TimeZone, Utc, Weekday};
-use pfm_core::forex::interface::ForexStorage;
+use pfm_core::forex::interface::{ForexHistoricalRates, ForexStorage};
 use pfm_core::forex::{service, ForexError};
 use pfm_core::forex_impl::forex_storage::ForexStorageImpl;
-use pfm_core::forex_impl::open_exchange_api;
 use pfm_core::global;
 use pfm_core::{
     forex::ForexResult, forex_impl::currency_api::Api as CurrencyAPI,
@@ -10,20 +9,59 @@ use pfm_core::{
     forex_impl::open_exchange_api::Api as OpenExchangeRatesAPI,
 };
 use std::time::Duration;
-use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    let from = Utc.with_ymd_and_hms(2000, 1, 1, 0, 0, 0).unwrap();
-    let to = Utc.with_ymd_and_hms(2024, 12, 31, 0, 0, 0).unwrap();
-    // println!("{:?} {}", weekdays(from, to), weekdays(from, to).len());
-    let oxrapi = open_exchange_api::Api::new(
-        &global::config().forex_open_exchange_api_key,
-        global::http_client(),
-    );
     let storage = ForexStorageImpl::new(global::storage_fs());
-    let ret = fetch_historical_data(Apis::OpenExchangeRatesAPI(oxrapi), storage, from, to).await;
+    let latest_historical =
+        ForexStorage::get_historical_list(&storage, 1, 1, pfm_core::forex::entity::Order::DESC)
+            .await
+            .unwrap();
+    let start_date = {
+        if !latest_historical.rates_list.is_empty() {
+            latest_historical.rates_list[0].data.date
+        } else {
+            Utc.with_ymd_and_hms(2000, 1, 1, 0, 0, 0).unwrap()
+        }
+    };
+    println!("{}", start_date);
+
+    // let from = start_date;
+    let from = Utc.with_ymd_and_hms(2003, 9, 29, 0, 0, 0).unwrap();
+    let to = Utc.with_ymd_and_hms(2024, 12, 31, 0, 0, 0).unwrap();
+    let storage = ForexStorageImpl::new(global::storage_fs());
+    let apiname = ApisName::CurrencyAPI;
+    let forex_api = select_api(apiname);
+    let ret = fetch_historical_data(forex_api, storage, from, to).await;
     println!("{:?}", ret);
+}
+
+fn select_api(apiname: ApisName) -> Apis {
+    match apiname {
+        ApisName::ExchangeAPI => {
+            unimplemented!()
+        }
+        ApisName::CurrencyAPI => {
+            let currency_api = CurrencyAPI::new(
+                &global::config().forex_currency_api_key,
+                global::http_client(),
+            );
+            Apis::CurrencyAPI(currency_api)
+        }
+        ApisName::OpenExchangeRatesAPI => {
+            let oxrapi = OpenExchangeRatesAPI::new(
+                &global::config().forex_open_exchange_api_key,
+                global::http_client(),
+            );
+            Apis::OpenExchangeRatesAPI(oxrapi)
+        }
+    }
+}
+
+pub enum ApisName {
+    ExchangeAPI,
+    CurrencyAPI,
+    OpenExchangeRatesAPI,
 }
 
 #[derive(Clone)]
@@ -62,62 +100,110 @@ async fn fetch_historical_data(
 ) -> ForexResult<()> {
     match api {
         Apis::OpenExchangeRatesAPI(api) => {
-            let weekdays = weekdays(from, to);
             let quota_remaining = api.status().await?.data.usage.requests_remaining;
-            if quota_remaining <= 0 {
-                return Err(ForexError::internal_error("no quota remained"));
-            } else {
-                let total_requests = std::cmp::min(weekdays.len() as u32, quota_remaining);
-                let rate_limit = 4; // Requests per second
-
-                let mut completed_requests = 0;
-
-                let mut weekday_index = 0 as usize;
-
-                while completed_requests < total_requests {
-                    let batch_size = (total_requests - completed_requests).min(rate_limit);
-
-                    let mut handles = Vec::new();
-                    for _ in 0..batch_size {
-                        let api_ref = api.clone();
-                        let storage_ref = storage.clone();
-                        let date = weekdays[weekday_index];
-                        handles.push(tokio::spawn(async move {
-                            let ret = service::poll_historical_rates(
-                                &api_ref,
-                                &storage_ref,
-                                date,
-                                global::BASE_CURRENCY,
-                            )
-                            .await;
-                            println!("{}. Result date {}: {:?}", weekday_index, date, ret);
-                        }));
-                        weekday_index += 1;
-                    }
-
-                    for handle in handles {
-                        let _ = handle.await;
-                    }
-
-                    completed_requests += batch_size;
-
-                    sleep(Duration::from_secs(1)).await;
-                }
-            }
-
-            let latest = storage
-                .get_historical_list(1, 1, pfm_core::forex::entity::Order::DESC)
-                .await?;
-            if latest.rates_list.len() > 0 {
-                println!(
-                    "fetch_historical_data latest date fetched: {}",
-                    latest.rates_list[0].data.date
-                );
-            } else {
-                println!("no data fetched");
-            }
+            // 4 reqs/sec
+            let rate_limit = 4;
+            let seconds_per_batch = 1;
+            let ret = fetch_historical_rates_data(
+                api,
+                storage,
+                from,
+                to,
+                quota_remaining,
+                rate_limit,
+                seconds_per_batch,
+            )
+            .await;
+            ret
+        }
+        Apis::CurrencyAPI(api) => {
+            let quota_remaining = api.status().await?.quotas.month.remaining;
+            // 9 reqs/minute
+            let rate_limit = 10;
+            let seconds_per_batch = 62;
+            let ret = fetch_historical_rates_data(
+                api,
+                storage,
+                from,
+                to,
+                quota_remaining,
+                rate_limit,
+                seconds_per_batch,
+            )
+            .await;
+            ret
         }
         _ => return Err(ForexError::internal_error("not implemented yet")),
+    }
+}
+
+async fn fetch_historical_rates_data<A, S>(
+    forex_api: A,
+    storage: S,
+    from: DateTime<Utc>,
+    to: DateTime<Utc>,
+    quota_remaining: u32,
+    rate_limit: u32,
+    seconds_per_batch: u32,
+) -> ForexResult<()>
+where
+    A: ForexHistoricalRates + Clone + Send + Sync + 'static,
+    S: ForexStorage + Clone + Send + Sync + 'static,
+{
+    let weekdays = weekdays(from, to);
+    if quota_remaining <= 0 {
+        return Err(ForexError::internal_error("no quota remained"));
+    }
+
+    let total_requests = std::cmp::min(weekdays.len() as u32, quota_remaining);
+    let sleep = seconds_per_batch as u64;
+
+    let mut completed_requests = 0;
+    let mut weekday_index = 0;
+
+    while completed_requests < total_requests && weekday_index < weekdays.len() {
+        let batch_size = std::cmp::min(total_requests - completed_requests, rate_limit);
+        let batch_size = std::cmp::min(batch_size, weekdays.len() as u32 - weekday_index as u32);
+
+        let mut handles = Vec::new();
+        for _ in 0..batch_size {
+            let api_clone = forex_api.clone();
+            let storage_clone = storage.clone();
+            let date = weekdays[weekday_index];
+            let index = weekday_index;
+
+            handles.push(tokio::spawn(async move {
+                let ret = service::poll_historical_rates(
+                    &api_clone,
+                    &storage_clone,
+                    date,
+                    global::BASE_CURRENCY,
+                )
+                .await;
+                println!("{}. Result date {}: {:?}", index, date, ret);
+            }));
+            weekday_index += 1;
+        }
+
+        for handle in handles {
+            let _ = handle.await;
+        }
+
+        completed_requests += batch_size;
+
+        tokio::time::sleep(Duration::from_secs(sleep)).await;
+    }
+
+    let latest = storage
+        .get_historical_list(1, 1, pfm_core::forex::entity::Order::DESC)
+        .await?;
+    if !latest.rates_list.is_empty() {
+        println!(
+            "fetch_historical_data latest date fetched: {}",
+            latest.rates_list[0].data.date
+        );
+    } else {
+        println!("no data fetched");
     }
 
     Ok(())
