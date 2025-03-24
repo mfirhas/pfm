ffd4a49c5d9e27a95d3dc833b2adb7792036eb6a
mfirhas <fathir.irhas@gmail.com>
2025-03-24 20:51:02 +0700

    Add insert batch historical integ test

--- pfm-core/tests/test_storage.rs
+++ pfm-core/tests/test_storage.rs
@@ -1,7 +1,10 @@
 use chrono::{TimeZone, Utc};
 use pfm_core::{
-    forex::{interface::ForexStorage, Money},
-    forex_impl::forex_storage::ForexStorageImpl,
+    forex::{
+        interface::{ForexStorage, ForexTimeseriesRates},
+        Money,
+    },
+    forex_impl::{self, forex_storage::ForexStorageImpl},
     global,
 };
 use rust_decimal_macros::dec;
@@ -28,4 +31,24 @@ pub async fn test_storage_update_historical() {
 
     assert_eq!(after.as_ref().unwrap().data.rates.xau, new_data[0].amount());
     assert_eq!(after.as_ref().unwrap().data.rates.xag, new_data[1].amount());
+}
+
+#[tokio::test]
+pub async fn test_storage_insert_batch() {
+    let api = forex_impl::currencybeacon::Api::new(
+        &global::config().forex_currencybeacon_api_key,
+        global::http_client(),
+    );
+
+    let start_date = Utc.with_ymd_and_hms(1996, 1, 1, 0, 0, 0).unwrap();
+    let end_date = Utc.with_ymd_and_hms(1996, 1, 12, 0, 0, 0).unwrap();
+
+    let ret = api
+        .timeseries_rates(start_date, end_date, global::BASE_CURRENCY)
+        .await;
+    dbg!(&ret);
+
+    let storage_impl = ForexStorageImpl::new(global::storage_fs());
+    let ret = ForexStorage::insert_historical_batch(&storage_impl, ret.unwrap()).await;
+    dbg!(&ret);
 }
