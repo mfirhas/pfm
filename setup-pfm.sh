a5aefd2502ceea72ba45e1ad56b06a7ab21100f3
mfirhas <fathir.irhas@gmail.com>
2025-04-21 00:16:55 +0700

    Add setup log files

--- setup-pfm.sh
+++ setup-pfm.sh
@@ -13,6 +13,33 @@ set -e
 
 echo "Current user: $(whoami)"
 
+### SETTING LOG FILES
+echo "setting log files"
+PFM_HTTP_STDOUT_LOG_FILE="/var/log/pfm-http.log"
+PFM_CRON_STDOUT_LOG_FILE="/var/log/pfm-cron.log"
+
+echo "setting '$PFM_HTTP_STDOUT_LOG_FILE' file..."
+if [ -f "$PFM_HTTP_STDOUT_LOG_FILE" ]; then
+    echo "File '$PFM_HTTP_STDOUT_LOG_FILE' exists."
+else
+    echo "File '$PFM_HTTP_STDOUT_LOG_FILE' does not exist. Creating it..."
+    sudo touch "$PFM_HTTP_STDOUT_LOG_FILE"
+    echo "File '$PFM_HTTP_STDOUT_LOG_FILE' created."
+fi
+
+echo "setting '$PFM_CRON_STDOUT_LOG_FILE' file..."
+if [ -f "$PFM_CRON_STDOUT_LOG_FILE" ]; then
+    echo "File '$PFM_CRON_STDOUT_LOG_FILE' exists."
+else
+    echo "File '$PFM_CRON_STDOUT_LOG_FILE' does not exist. Creating it..."
+    sudo touch "$PFM_CRON_STDOUT_LOG_FILE"
+    echo "File '$PFM_CRON_STDOUT_LOG_FILE' created."
+fi
+sudo chown pfm:pfm $PFM_HTTP_STDOUT_LOG_FILE $PFM_CRON_STDOUT_LOG_FILE
+sudo chmod 644 $PFM_HTTP_STDOUT_LOG_FILE $PFM_CRON_STDOUT_LOG_FILE
+### END
+
+### SETTING PFM DIRECTORIES
 if [ ! -d "$NEW_PATH" ]; then
     echo "📁 Directory $NEW_PATH does not exist. Creating: $NEW_PATH"
     mkdir -p "$NEW_PATH"
@@ -36,6 +63,7 @@ if [ ! -d "$PFM_DATA" ]; then
 else
     echo "✅ Directory already exists: $PFM_DATA"
 fi
+### END
 
 echo "Changing owner of $PFM_DATA"
 sudo chown pfm:pfm $PFM_DATA
